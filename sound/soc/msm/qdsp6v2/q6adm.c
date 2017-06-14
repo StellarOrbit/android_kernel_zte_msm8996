80a74e2afeb4e0b069cf6a95312e9a79381550cd
c457 <android.c357@gmail.com>
2017-06-14 00:07:46 -0500

    here there everywhere

--- sound/soc/msm/qdsp6v2/q6adm.c
+++ sound/soc/msm/qdsp6v2/q6adm.c
@@ -2380,9 +2380,6 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 	copp_idx = adm_get_idx_if_copp_exists(port_idx, topology, perf_mode,
 						rate, bit_width, app_type);
 
-    	pr_err("%s: chenjun: copp_idx-%d, rate-%d, bit_width-%d, app_type-%#X\n",
-		 __func__, copp_idx, rate, bit_width, app_type);
-
 	if (copp_idx < 0) {
 		copp_idx = adm_get_next_available_copp(port_idx);
 		if (copp_idx >= MAX_COPPS_PER_PORT) {
